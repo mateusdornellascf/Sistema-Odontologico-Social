9acea1428ceec410b3c9cda849c23c82d82de7f1
mateusdornellascf <mateusdornellascf@gmail.com>
2026-03-27 14:34:37 -0300

    Aplicação springboot

---
+++ clinica-odontologica-social/src/main/java/br/com/bd/projeto/cesar/clinica_odontologica_social/ClinicaOdontologicaSocialApplication.java
@@ -0,0 +1,13 @@
+package br.com.bd.projeto.cesar.clinica_odontologica_social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaOdontologicaSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaOdontologicaSocialApplication.class, args);
+	}
+
+}
---
+++ clinica-odontologica-social/src/test/java/br/com/bd/projeto/cesar/clinica_odontologica_social/ClinicaOdontologicaSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bd.projeto.cesar.clinica_odontologica_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaOdontologicaSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
