aadac921d8fd6f097c48104722671046b397da93
mateusdornellascf <mateus.dornellascf@gmail.com>
2026-05-23 13:40:10 -0300

    adiciona no controller métodos para deletar formulários por CPF e ID

--- clinica-odontologica-social/src/main/java/br/com/bd/projeto/cesar/clinica_odontologica_social/controllers/FormularioSaudeController.java
+++ clinica-odontologica-social/src/main/java/br/com/bd/projeto/cesar/clinica_odontologica_social/controllers/FormularioSaudeController.java
@@ -3,6 +3,7 @@ package br.com.bd.projeto.cesar.clinica_odontologica_social.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,22 @@ public class FormularioSaudeController {
             return ResponseEntity.ok("Nenhum paciente com alerta de saúde encontrado.");
         }
         return ResponseEntity.ok(resultado);
+    }
+
+    @DeleteMapping("/paciente/{cpf}")
+    public ResponseEntity<String> deletarPorCpf(@PathVariable String cpf) {
+        boolean ok = formularioSaudeService.deletarPorCpf(cpf);
+        return ok
+                ? ResponseEntity.ok("Formulário(s) deletado(s) com sucesso.")
+                : ResponseEntity.ok("Nenhum formulário encontrado para este CPF.");
+    }
+
+    @DeleteMapping("/{idFormulario}")
+    public ResponseEntity<String> deletarPorId(@PathVariable int idFormulario) {
+        boolean ok = formularioSaudeService.deletarPorId(idFormulario);
+        return ok
+                ? ResponseEntity.ok("Formulário deletado com sucesso.")
+                : ResponseEntity.ok("Formulário não encontrado.");
     }
 
 }
